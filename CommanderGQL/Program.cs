6758ec02f008faed2bc02514ff695edd39903a48
Kyle Morton <kylemortondevelopment@gmail.com>
2021-12-18 21:37:52 -0600

    adding mig for platform

--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlServer(builder.Configuration["DefaultConnection"]);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddControllers();
